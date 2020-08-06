a6669266ed337b60f2582e0c2bbfd803aa77227b
Leeroy Phili <leeroyphili@gmail.com>
2020-08-06 01:26:23 -0400

    added prop-types to both components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class TodoItem extends Component {
+getStyle = () => {
+   return{
+       background: 'f4f4f4',
+       padding: '10px',
+       borderBotton: '1px #ccc dotted',
+       textDecoration: this.props.todo.completed? 
+       'line-through' : 'none'
+   }
+    
+}
+
     render() {
         return (
-            <div>
+            <div style= {this.getStyle()}>
               <p>{this.props.todo.tittle}</p>  
             </div>
         )
     }
 }
+        //PropTypes
+  TodoItem.propTypes = {
+    todos: PropTypes.object.isRequired
+}  
+
+
 
 export default TodoItem
